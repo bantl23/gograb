dfe6d5e0cbc168117f040124e78024b525d31be7
Deepak Kaul <deepak.b.kaul@gmail.com>
2020-08-11 01:34:15 +0000

    Initial commit of gograb.
    gograb pulls golang modules one at a time listed in go.mod in the
    current directory. This is to fix a ci/cd issue and issue where
    private repositories limit the number of concurrent connections.

---
+++ go.mod
@@ -0,0 +1,5 @@
+module aetd-git.gsfc.nasa.gov/daphne/golang/utils/gograb
+
+go 1.14
+
+require golang.org/x/mod v0.3.0
---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+
+	"golang.org/x/mod/modfile"
+)
+
+const (
+	gomod = "go.mod"
+)
+
+var (
+	version = "v0.0.0"
+	commit  = "unknown"
+)
+
+func main() {
+	if len(os.Args) == 2 {
+		if os.Args[1] == "-v" || os.Args[1] == "--version" {
+			fmt.Printf("%s (%s)\n", version, commit)
+			os.Exit(0)
+		}
+		if os.Args[1] == "-h" || os.Args[1] == "--help" {
+			fmt.Println("Downloads go modules one at a time")
+			os.Exit(0)
+		}
+	}
+
+	contents, err := ioutil.ReadFile(gomod)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	file, err := modfile.Parse(gomod, contents, nil)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	for j := range file.Require {
+		modvers := fmt.Sprintf("%+v@%+v", file.Require[j].Mod.Path, file.Require[j].Mod.Version)
+		cmdGoGet := exec.Command("go", "get", "-d", modvers)
+		fmt.Printf("downloading %s\n", modvers)
+		resultGoGet, err := cmdGoGet.CombinedOutput()
+		if err != nil {
+			fmt.Printf("%s [%+v]\n", resultGoGet, err)
+			os.Exit(1)
+		}
+	}
+}
